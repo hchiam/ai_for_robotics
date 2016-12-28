092f557263d7c96218c0160d58cf8dcbce1e3e0c
hchiam <hchiam@gmail.com>
2016-12-28 14:15:45 -0500

    add predict step

--- kalmanFilter.py
+++ kalmanFilter.py
@@ -1,3 +1,6 @@
+# MEASUREMENT (SENSING) UPDATE STEP OF KALMAN FILTER:
+# (BAYES RULE MULTIPLICATION)
+
 # Write a program to update your mean and variance
 # when given the mean and variance of your belief
 # and the mean and variance of your measurement.
@@ -9,4 +12,22 @@ def update(mean1, var1, mean2, var2):
     new_var = 1 / (1/var1 + 1/var2)
     return [new_mean, new_var]
 
-print update(10.,8.,13., 2.)
+print update(10., 8., 13., 2.)
+
+
+# MOTION UPDATE STEP OF KALMAN FILTER:
+# (TOTAL PROBABILITY ADDITION)
+
+# Write a program that will predict your new mean
+# and variance given the mean and variance of your 
+# prior belief and the mean and variance of your 
+# motion. 
+
+# PREDICTION STEP:
+
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+print predict(10., 4., 12., 4.)
