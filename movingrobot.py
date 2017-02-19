bf3e82de5581bc897673081e78696ad94d29b80b
hchiam <hchiam@gmail.com>
2017-02-19 14:44:11 -0500

    add robot error with print outs

--- movingrobot.py
+++ movingrobot.py
@@ -101,6 +101,8 @@ class robot:
 
 
 
+# output: error with respect to distance to landmarks (assumes "cyclic" world)
+# parameters: r = robot position ; p = particle list
 def eval(r, p):
     sum = 0.0;
     for i in range(len(p)): # calculate mean error
@@ -211,4 +213,43 @@ for step in range(t):
     
     print("(Done.)")
 
-print("\nDone iterations of move, sense, etc.\n")
+
+print("\nRobot error print-outs:\n")
+
+myrobot = robot()
+myrobot = myrobot.move(0.1, 5.0)
+Z = myrobot.sense()
+N = 1000
+T = 10 #Leave this as 10 for grading purposes.
+
+p = []
+for i in range(N):
+    r = robot()
+    r.set_noise(0.05, 0.05, 5.0)
+    p.append(r)
+
+for t in range(T):
+    myrobot = myrobot.move(0.1, 5.0)
+    Z = myrobot.sense()
+
+    p2 = []
+    for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+    p = p2
+
+    w = []
+    for i in range(N):
+        w.append(p[i].measurement_prob(Z))
+
+    p3 = []
+    index = int(random.random() * N)
+    beta = 0.0
+    mw = max(w)
+    for i in range(N):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % N
+        p3.append(p[index])
+    p = p3
+    print(eval(myrobot, p))
