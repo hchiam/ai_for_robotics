dcacd438c3fe7525d958eb410df72d5e381e2a6a
hchiam <hchiam@gmail.com>
2017-04-21 17:35:49 -0400

    car lane region detection

---
+++ car_lane_region.py
@@ -0,0 +1,42 @@
+# From lesson 2 of free preview of Self-Driving Car Engineer Nanodegree from Udacity.
+# I basically put things in my own words for clearer reading.
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# get image, print stats
+img = mpimg.imread('image.jpg')
+print('this image is ', type(img), ' with dimensions ', img.shape)
+
+# get dimensions
+xsize = img.shape[1]
+ysize = img.shape[0]
+
+# get copy of img, make into array (why copy? (habit: b/c py is pass-by-ref))
+region_select = np.copy(img)
+
+# get triangle corner coordinates
+# (top left = [row=0, col=0]) ; like typical x and y but y is flipped
+left = [0, 540]
+right = [960, 540]
+top = [480, 260]
+
+# get fit lines (y=mx+b) as the sides of the triangle
+# np.polyfit() returns [m, b]
+fit_left = np.polyfit((left[0], top[0]), (left[1], top[1]), 1)
+fit_right = np.polyfit((right[0], top[0]), (right[1], top[1]), 1)
+fit_bottom = np.polyfit((left[0], right[0]), (left[1], right[1]), 1)
+
+# get rectangular meshgrid for x and y
+x,y = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+
+# get pixels that are within the triangle
+pixels_to_change = (y > (fit_left[0] * x + fit_left[1])) & (y > (fit_right[0] * x + fit_right[1])) & (y < (fit_bottom[0] * x + fit_bottom[1]))
+
+# set those pixels to red in the corresponding copied array
+region_select[pixels_to_change] = [255, 0, 0]
+
+# show filtered image
+plt.imshow(region_select)
+plt.show()
